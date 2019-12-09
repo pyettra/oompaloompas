db4865335cf6cdc6e5bf145cb65032bb14c2412c
Pyettra Ferreira <pyettraf@gmail.com>
2019-12-09 08:34:20 -0300

    Juntando as partes

--- OompaLoompa/Controller/ChildrenViewController.swift
+++ OompaLoompa/Controller/ChildrenViewController.swift
@@ -10,10 +10,20 @@ import Foundation
 import UIKit
 
 class ChildrenViewController: UIViewController {
-    
+
+    @IBOutlet weak var lettersCollection: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lettersCollection.delegate = self
+        lettersCollection.dataSource = self
+        lettersCollection.register(UINib(nibName: "LetterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "letterCell")
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadCollection), name: NSNotification.Name("updateLetters"), object: nil)
+    }
+    
+    @objc func reloadCollection(_ notification: Notification) {
+        lettersCollection.reloadData()
     }
     
     
@@ -40,5 +50,45 @@ class ChildrenViewController: UIViewController {
 //        self.show(navigationController, sender: self)
     }
     
+}
+
+extension ChildrenViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Model.instance.children[0].letters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "letterCell", for: indexPath) as? LetterCollectionViewCell {
+            cell.configure(letter: Model.instance.children[0].letters[indexPath.row])
+            cell.layer.masksToBounds = true
+            cell.layer.cornerRadius = cell.frame.width/10.0
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+       //navegar para a modal de desenho, áudio ou carta
+        
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 0)
+    }
+    
     
 }
+
